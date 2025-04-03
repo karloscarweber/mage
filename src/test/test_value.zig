5b2f1d8f4087bb5be94dec6325e11673f0d7ea9c
Karl Weber <me@kow.fm>
2025-04-02 22:49:14 -0600

    Cleaned up the value primitive some more. Better interface.

--- src/test/test_value.zig
+++ src/test/test_value.zig
@@ -7,35 +7,72 @@ const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
 const testing = std.testing;
 const print = std.debug.print;
+// const NIL = vl.NIL;
 
 // TESTS
 const expect = testing.expect;
 
 // Parse a value into a value.
 
-test "Values can be created" {
-    const val = Value.ValueAs{ .int = 0 };
-    const nilValueOne = Value.new(Value.Type.Nil, val);
-    
-    const nilValue = Value.init();
-    
-    const val2 = Value.ValueAs{ .int = 15 };
-    const intValue = Value.new(Value.Type.Int, val2);
 
-    try expect(nilValueOne.isNil());
+
+pub const Ass = union(enum) {
+    int: i64,
+    float: f64,
+    boolean: bool,
+    nil,
+};
+
+test "Figure out unions" {
+    const thing = Ass{.int = 10};
+    try expect(thing.int == 10);
+}
+
+
+test "Values can be created" {
+    
+    const nilValue = Value.new(.nil);
+    const intValue = Value.init(.{.int = 15});
+    const floatValue = Value.init(.{.float = 15.55});
+
     try expect(nilValue.isNil());
     try expect(intValue.isNumber());
+    try expect(floatValue.isNumber());
+
     const numberValue = 15;
     try expect(intValue.as.int == numberValue);
+
+    const floatTest = 15.55;
+    try expect(floatValue.as.float == floatTest);
+}
+
+test "Bool Values can be created" {
+    const boolValue = Value.init(.{ .boolean = false});
+    try expect(boolValue.isFalsy());
+    try expect(!boolValue.isTruthy());
     
-    // make some strings with values
+    const trueValue = Value.init(.{ .boolean = true});
+    try expect(trueValue.isTruthy());
+    
+    const falseValue = Value.init(.{ .boolean = false});
+    try expect(falseValue.isFalsy());
+    
+    const wrong = Value{ .as = .{.boolean = false}};
+    try expect(wrong.isFalsy());
+    
+    const whatever = Value.new(.{.boolean = false});
+    try expect(whatever.isFalsy());
+}
+
+test "Values can be made from strings" {
+    // make values with strings
     // 15
     // 19.99
     // "you suck"
     // main
     // true
     // false
-
+    
     // Expected values:
     //
     // value { type: .Int, as: ValueAs{.int: 15}}
--- src/value.zig
+++ src/value.zig
@@ -4,60 +4,83 @@
 // Types of Values:
 //  * Integer
 //  * Float
-//  * String
-//  * Object - maybe not use this? I don't know
+//  * Boolean
+//  * Nil
 //  * Function
 //
-
-// const Obj = struct {
-//     name: []const u8,
-// };
 
 const std = @import("std");
 const ArrayList = std.ArrayList;
 
 pub const Value = struct {
-    type: Type,
-    as: ValueAs,
+    as: As,
 
     const Self = @This();
 
-    pub const Type = enum {
-        Nil,
-        Bool,
-        Int,
-        Float,
-    };
-    
-    pub const ValueAs = union {
+    pub const As = union(enum) {
         int: i64,
-        float: f64
+        float: f64,
+        boolean: bool,
+        nil: void,
     };
 
-    // Value.init()
-    pub fn init() Value {
-        return .{
-            .type = Type.Nil,
-            .as = ValueAs{ .int = 0 }
-        };
+    // makes a new Value:
+    //
+    // const value1 = Value.init(.nil);
+    // const value2 = Value.init(.{.int = 15});
+    // const value3 = Value.init(.{.float = 0.99});
+    pub fn init(value: As) Value {
+        return .{.as = value};
     }
     
-    pub fn new(tyyp: Type, value: ValueAs) Value {
-        return .{
-            .type = tyyp,
-            .as = value
-        };
+    pub fn new(value: As) Value {
+        return Value.init(value);
     }
-    
-    // pub fn deinit(self: *Self) void {
-    //     // release everything.
-    //     // self.lexer.deinit();
-    // }
-    
+
     // value.isBool(), or .isNumber(), returns true or false statement thingys
-    pub fn isBool(self: Self) bool { return (self.type == Type.Bool);}
-    pub fn isNil(self: Self) bool { return (self.type == Type.Nil);}
-    pub fn isNumber(self: Self) bool { return (self.type == Type.Int or self.type == Type.Float);}
+    pub fn isBool(self: Self) bool { 
+        switch (self.as) {
+            .boolean => return true,
+            else => return false,
+        }
+    }
+    
+    pub fn isNil(self: Self) bool { 
+        switch (self.as) {
+            .nil => return true,
+            else => return false,
+        }
+    }
+    
+    pub fn isNumber(self: Self) bool { 
+        switch (self.as) {
+            .float, .int => return true,
+            else => return false,
+        }
+    }
+    
+    pub fn isFalsy(self: Self) bool { 
+        const it = self.as;
+        switch (it) {
+            .float => |floating_point| {
+                if (floating_point > 0) { return false; }
+                
+            }, .int => |integer| {
+                if (integer > 0) { return false; }
+                
+            },
+            .boolean => |bl| {
+                if (bl == true) { return false; }
+            },
+            else => return false,
+        }
+        return true;
+    }
+    
+    pub fn isTruthy(self: Self) bool { 
+        return (!self.isFalsy());
+    }
+
 };
 
 pub const Values = ArrayList(Value);
